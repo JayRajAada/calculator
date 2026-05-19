22cf89cce5d38862215f0b2685a179ac7324876b
JayRajAada <jayrajaada@gmail.com>
2026-05-19 17:10:01 +0530

    Implemented operate function logic

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function divide(a , b){
     }
     return a/b;
 }
+
+let firstOperand,operator,secondOperand;
+
+function operate(first , second , op){
+    let result = 0;
+    if(op === '+'){
+        result = add(first , second);
+    }
+    else if(op === '-'){
+        result = subtract(first , second);
+    }
+    else if(op === '*'){
+        result = multiply(first , second);
+    }
+    else if(op === '/'){
+       result = divide(first , second);
+    }
+    else{
+        return "Operator Doesn't Exist";
+    }
+    return result;
+}
