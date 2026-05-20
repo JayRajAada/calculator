c0a08228f0ed5c4c23b23366796bf15b61ce712a
JayRajAada <jayrajaada@gmail.com>
2026-05-20 13:12:02 +0530

    Implemented basic calculator evaluation

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
-function add(a , b){
+function add(a, b) {
     return a + b;
 }
-function subtract(a , b){
+function subtract(a, b) {
     return a - b;
 }
-function multiply(a , b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
-function divide(a , b){
-    if(b === 0){
+function divide(a, b) {
+    if (b === 0) {
         return "Can't divide by Zero !";
     }
-    return a/b;
+    return a / b;
 }
 
-let firstOperand,operator,secondOperand;
+let firstOperand, operator, secondOperand;
 
-function operate(first , second , op){
+function operate(first, second, op) {
     let result = 0;
-    if(op === '+'){
-        result = add(first , second);
+    if (op === '+') {
+        result = add(first, second);
     }
-    else if(op === '-'){
-        result = subtract(first , second);
+    else if (op === '-') {
+        result = subtract(first, second);
     }
-    else if(op === '*'){
-        result = multiply(first , second);
+    else if (op === '*') {
+        result = multiply(first, second);
     }
-    else if(op === '/'){
-       result = divide(first , second);
+    else if (op === '/') {
+        result = divide(first, second);
     }
-    else{
+    else {
         return "Operator Doesn't Exist";
     }
     return result;
 }
+
+let displayValue = "";
+const operators = document.querySelectorAll('.operators button');
+const numbtn = document.querySelectorAll('.number button');
+const display = document.querySelector('#display');
+
+numbtn.forEach((singleItem) => {
+    singleItem.addEventListener('click', (event) => {
+        let text = event.target.innerText;
+        displayValue += text;
+        display.textContent = displayValue;
+    });
+});
+
+operators.forEach((singleItem) => {
+    singleItem.addEventListener('click', (event) => {
+        if (event.target.innerText === '=') {
+            secondOperand = displayValue;
+            displayValue = operate(Number(firstOperand), Number(secondOperand), operator);
+            display.textContent = displayValue;
+        }
+        else {
+            firstOperand = displayValue;
+            operator = event.target.innerText;
+            display.textContent = operator;
+            displayValue = "";
+        }
+    });
+});
+
+
+
